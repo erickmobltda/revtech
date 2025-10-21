d09bbfc0851d0fa6f915fb75732ed97bbee969ee
erickmob <erickmobltda@gmail.com>
2025-10-21 16:26:15 -0300

    improved cart pt2

--- src/components/FloatingCartButton.tsx
+++ src/components/FloatingCartButton.tsx
@@ -18,7 +18,7 @@ const FloatingCartButton: React.FC<FloatingCartButtonProps> = ({ itemCount, onCl
     >
       <div className="cart-icon">
         <svg width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2.5 5M7 13l2.5 5m6-5v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6m8 0V9a2 2 0 00-2-2H9a2 2 0 00-2 2v4.01"></path>
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
         </svg>
         {itemCount > 0 && (
           <span className="cart-badge">{itemCount}</span>
